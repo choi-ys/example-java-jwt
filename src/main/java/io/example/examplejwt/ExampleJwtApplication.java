cdf74b81a00d28057d59f7f08a167244f2164599
choi-ys <project.log.062@gmail.com>
2021-08-31 15:29:00 +0900

    [2021-08-31]용석:Initial commit

---
+++ src/main/java/io/example/examplejwt/ExampleJwtApplication.java
@@ -0,0 +1,13 @@
+package io.example.examplejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/example/examplejwt/ExampleJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.example.examplejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
