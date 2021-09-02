4d2353da635789473c89de4e5c0cd20ced4463ac
choy-ys <project.log.062@gmail.com>
2021-09-02 20:53:59 +0900

    [2021-09-02]용석:회원 가입 시, 이메일 항목 중복 검사를 위한 Repository Method 추가

--- src/main/java/io/example/jwt/repository/MemberRepository.java
+++ src/main/java/io/example/jwt/repository/MemberRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @date : 2021/09/02 9:52 오전
  */
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmail(String email);
 }
