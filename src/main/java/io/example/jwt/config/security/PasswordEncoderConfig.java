fba8cbba134c27fd781948da6a27dd394394df5f
choy-ys <project.log.062@gmail.com>
2021-09-02 20:51:38 +0900

    [2021-09-02]용석:회원 가입 시, 비밀번호 항목 암호화를 위한 PasswordEncoder Bean 설정

---
+++ src/main/java/io/example/jwt/config/security/PasswordEncoderConfig.java
@@ -0,0 +1,19 @@
+package io.example.jwt.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author : choi-ys
+ * @date : 2021/09/02 3:31 오후
+ */
+@Configuration
+public class PasswordEncoderConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
